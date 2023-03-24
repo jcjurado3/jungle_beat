31deb50e17c022d7e65ac41493dbfd6c1ddcde60
Jimmy Jurado <jurado140@gmail.com>
2023-03-23 23:14:22 -0400

    Add the completed node class and start of LinkedList class building with tests.

---
+++ lib/linked_list.rb
@@ -0,0 +1,12 @@
+class LinkedList
+  attr_reader :head
+  def initialize
+    @head = head
+  end
+  def append(data)
+    @head = Node.new(data)
+  end
+  def count
+
+  end
+end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,1 +1,34 @@
-So you linked_list class will have append, count, to_string, prepend, insert, find, includes?, and pop methodsSo you linked_list class will have append, count, to_string, prepend, insert, find, includes?, and pop methods
+require 'rspec'
+require "./lib/node"
+require "./lib/linked_list"
+
+RSpec.describe Node do
+
+  it 'testing for data and next node attributes' do
+    node = Node.new("plop")
+
+    expect(node.data).to eq("plop")
+    expect(node.next_node).to eq(nil)
+  end
+  it 'testing for the Linked List Class' do
+
+    list = LinkedList.new
+    expect(list).to be_a(LinkedList)
+    expect(list.head).to eq(nil)
+  end
+
+ it 'testing for the append method' do
+  # Set up below
+  list = LinkedList.new
+  # Assertion below
+  list.append("doop")
+  require 'pry'; binding.pry
+  expect(list.head.data).to eq("doop")
+  expect(list.head.next_node).to eq(nil)
+  expect(list.count).to eq(1)
+
+  expect(list.to_string).to eq("doop")
+ end
+
+ 
+end
