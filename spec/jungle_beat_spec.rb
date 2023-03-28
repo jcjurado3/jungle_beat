eb710fddf02738e23debaed7166cf3d95eebb82f
Jimmy Jurado <jurado140@gmail.com>
2023-03-28 09:45:37 -0400

    Fix some errors and split spec files into separate files.

---
+++ spec/jungle_beat_spec.rb
@@ -0,0 +1,53 @@
+require "rspec"
+require "./lib/node"
+require "./lib/linked_list"
+require "./lib/jungle_beat"
+
+RSpec.describe JungleBeat do
+
+  it 'creates the JungleBeat Linked List "Wrapper" class' do
+
+    jb = JungleBeat.new
+  
+  
+    expect(jb.list).to be_a(LinkedList)
+    expect(jb.list.head).to eq(nil)
+  
+    jb.append("deep doo ditt")
+    expect(jb.list.head.data).to eq("deep")
+    expect(jb.list.head.next_node.data).to eq("doo")
+  
+    jb.append("woo hoo shu")
+    expect(jb.count).to eq(6)
+   end
+  
+   it 'creates the play method' do
+    jb = JungleBeat.new
+    jb.append("deep doo ditt woo hoo shu")
+    # expect(jb.count).to eq(6)
+    # expect(jb.list.count).to eq(6)
+    expect(jb.play).to eq("")
+   end
+   it 'creates the all method' do
+    jb = JungleBeat.new("deep")
+    jb.append("Mississippi")
+  
+    expect(jb.all).to eq("Mississippi")
+  
+   end
+   it 'creates the rate method' do
+  
+    jb = JungleBeat.new
+    jb.append("deep dop dop deep")
+    jb.rate = 100
+  
+    expect(jb.play).to eq("")
+   end
+   it 'creates the voice method' do
+    jb = JungleBeat.new
+    jb.append("deep dop dop deep")
+    jb.rate = 50
+    jb.voice = "Daniel"
+    expect(jb.play).to eq("")
+   end
+end
---
+++ spec/linked_list_spec.rb
@@ -0,0 +1,72 @@
+require 'rspec'
+require "./lib/node"
+require "./lib/linked_list"
+
+RSpec.describe LinkedList do
+  it 'testing for the Linked List Class' do
+
+    list = LinkedList.new
+    expect(list).to be_a(LinkedList)
+    expect(list.head).to eq(nil)
+  end
+
+ it 'testing for append, count, and to string methods' do
+  # Set up below
+
+  list = LinkedList.new
+  list.append("doop")
+  # Assertion below
+
+
+  expect(list.head.data).to eq("doop")
+  expect(list.head.next_node).to eq(nil)
+  expect(list.count).to eq(1)
+
+  expect(list.to_string).to eq("doop")
+ end
+ it 'testing for multiple nodes' do
+  list = LinkedList.new
+  list.append("doop")
+  expect(list).to be_a(LinkedList)
+  list.append("deep")
+  expect(list.count).to eq(2)
+  expect(list.to_string).to eq("doop deep")
+ end
+ it 'testing for prepend method' do
+  list = LinkedList.new
+  list.append("plop")
+  # testing append
+  expect(list.head.data).to eq("plop")
+  list.append("suu")
+  list.prepend("dop")
+
+  expect(list.to_string).to eq("dop plop suu")
+ end
+ 
+ it 'testing for insert method' do
+  list = LinkedList.new
+  list.append("plop")
+  list.append("suu")
+  list.prepend("dop")
+  list.insert(1, "woo")
+  expect(list.to_string).to eq("dop woo plop suu")
+ end
+ it 'testing for find, pop and includes? methods' do
+  list = LinkedList.new
+  list.append("deep")
+  list.append("woo")
+  list.append("shi")
+  list.append("shu")
+  list.append("blop")
+
+  expect(list.to_string).to eq("deep woo shi shu blop")
+  expect(list.find(2,1 )).to eq("shi")
+  expect(list.find(1, 3)).to eq("woo shi shu")
+  expect(list.includes?("deep")).to be(true)
+
+  list.pop
+  list.pop
+  expect(list.to_string).to eq("deep woo shi")
+ end
+
+end
